d2cab820b26432fc42ac5fb023602953b0f66994
Paul Wong <kinyip1995@yahoo.com.hk>
2023-10-04 18:14:16 +0800

    add product.html v1

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import expressSession from 'express-session';
 import path from 'path';
-import { Client } from "pg";
-import dotenv from "dotenv";
+// import { Client } from "pg";
+// import dotenv from "dotenv";
 
 const app = express();
 
@@ -17,47 +17,48 @@ app.use(
 );
 
 
-dotenv.config();
-async function main() {
-    const client = new Client({
-        database: process.env.DB_NAME,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-    });
-    await client.connect();
-    await client.end();
-}
+// // #TODO Revise it later once SQL created
+// dotenv.config();
+// async function main() {
+//     const client = new Client({
+//         database: process.env.DB_NAME,
+//         user: process.env.DB_USERNAME,
+//         password: process.env.DB_PASSWORD,
+//     });
+//     await client.connect();
+//     await client.end();
+// }
 
-// #TODO Revise it later once SQL created
-import { ParsedQs } from "qs";
-function convertStr2Arr(query: string | string[] | ParsedQs | ParsedQs[] | undefined) {
-    if (typeof query === "undefined" || Array.isArray(query)) {
-        return query;
-    }
-    return [query];
-}
+// // #TODO Revise it later once SQL created
+// import { ParsedQs } from "qs";
+// function convertStr2Arr(query: string | string[] | ParsedQs | ParsedQs[] | undefined) {
+//     if (typeof query === "undefined" || Array.isArray(query)) {
+//         return query;
+//     }
+//     return [query];
+// }
 
-async function readJsonFile(filepath: string) {
-    const data = await jsonfile.readFile(filepath);
-    return data;
-}
-
-
-app.get("./products", async (req, res) => {
-    let products: Product[] = await readJsonFile(PRODUCT_JSON_PATH);
-    const categories = convertStr2Arr(req.query.category);
-    if (categories) {
-        products = products.filter((product) => categories.includes(product.category));
-    }
-    res.json(products);
-});
+// async function readJsonFile(filepath: string) {
+//     const data = await jsonfile.readFile(filepath);
+//     return data;
+// }
 
 
-// #TODO Revise it later once SQL created
-app.get("./categories", async (req, res) => {
-    const products: Product[] = await readJsonFile(PRODUCT_JSON_PATH);
-    res.json(Array.from(new Set(products.map((p) => p.category))));
-});
+// app.get("./products", async (req, res) => {
+//     let products: Product[] = await readJsonFile(PRODUCT_JSON_PATH);
+//     const categories = convertStr2Arr(req.query.category);
+//     if (categories) {
+//         products = products.filter((product) => categories.includes(product.category));
+//     }
+//     res.json(products);
+// });
+
+
+// // #TODO Revise it later once SQL created
+// app.get("./categories", async (req, res) => {
+//     const products: Product[] = await readJsonFile(PRODUCT_JSON_PATH);
+//     res.json(Array.from(new Set(products.map((p) => p.category))));
+// });
 
 
 
