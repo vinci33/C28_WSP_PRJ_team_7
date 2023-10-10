f7929276443297de72b0113ec10e16dd95491ce6
Paul Wong <kinyip1995@yahoo.com.hk>
2023-10-10 21:52:35 +0800

    debug shoppingCart.js

--- public/js/shoppingCart.js
+++ public/js/shoppingCart.js
@@ -41,8 +41,13 @@ async function initProducts() {
     productClone
       .querySelector(".remove-button")
       .addEventListener("click", function (e) {
-        const pid = e.target.getAttribute("pid");
-        deleteItems(pid);
+        if (
+          e.target.classList.contains("fa-xmark") ||
+          e.target.tagName === "B"
+        ) {
+          const pid = e.target.parentNode.getAttribute("pid");
+          deleteItems(pid);
+        }
       });
 
     productContainerEle.appendChild(productClone);
